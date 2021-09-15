923f00264dadfa7af948fd54cb749787d55484bb
Huferr <hugoferreirait@gmail.com>
2021-09-15 12:22:13 -0300

    feat: creating routes and screens

---
+++ src/pages/Settings/settings.screen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return 'blalba';
+};
+
+export default Settings;
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// pages
+
+import Home from '../pages/Dashboard/Home/home.screen';
+import Settings from '../pages/Settings/settings.screen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Dashboard: React.FC = () => {
+  return;
+};
+
+const App: React.FC = () => (
+  <Stack.Navigator
+    initialRouteName="dashboard"
+    screenOptions={{headerShown: false}}>
+    <Stack.Screen name="dashboard" component={Dashboard} />
+    <Stack.Screen name="settings" component={Settings} />
+  </Stack.Navigator>
+);
+
+export default App;
