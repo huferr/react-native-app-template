15e19cb19cdef72a35e2a2cdaeff9dab3fdf3e6f
Huferr <hugoferreirait@gmail.com>
2021-09-19 02:40:21 -0300

    feat: creating home icon

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,6 +7,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../pages/Dashboard/Home/home.screen';
 import Settings from '../pages/Settings/settings.screen';
 import Profile from '../pages/Dashboard/Profile/profile.screen';
+import HomeIcon from '../assets/icons/home';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -16,8 +17,8 @@ const Dashboard: React.FC = () => {
     <Tab.Navigator
       screenOptions={{
         tabBarShowLabel: false,
-        tabBarActiveTintColor: 'black',
-        tabBarInactiveTintColor: 'grayLight',
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#9c9c9c',
         headerShown: false,
         tabBarItemStyle: {
           // paddingVertical: isIos() ? 30 : 0,
@@ -38,7 +39,7 @@ const Dashboard: React.FC = () => {
         name="home"
         component={Home}
         options={{
-          // tabBarIcon:,
+          tabBarIcon: () => <HomeIcon />,
           headerShown: false,
         }}
       />
