3809502f1b39a0ed2f96bd333d3470c1f68b6c7d
Huferr <hugoferreirait@gmail.com>
2021-09-19 22:30:36 -0300

    feat: adding default colors

---
+++ src/styles/colors.tsx
@@ -0,0 +1,15 @@
+export default {
+  transparent: 'transparent',
+
+  warning: '#f3bA2f',
+  danger: '#ff6378',
+  success: '#00e699',
+
+  white: '#ffffff',
+  whiteLight: '#f8f9fb',
+  gray: '#f3f4f9',
+  grayLight: '#c3c6db',
+  grayRegular: '#9ea1b9',
+  grayMedium: '#757893',
+  black: '#000000',
+};
