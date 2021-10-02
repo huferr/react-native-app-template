36a365c2f151ec5323c2ba1a5af7ff18238f22f4
Huferr <hugoferreirait@gmail.com>
2021-10-01 22:48:00 -0300

    feat: creating home screen title

--- src/pages/Dashboard/Home/home.screen.tsx
+++ src/pages/Dashboard/Home/home.screen.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
 import Card from '../../../components/card/card.component';
 import {Container, Scroll} from '../../../styles/general';
+import {Title} from './home.styles';
 
 const Home: React.FC = () => {
+  const [userName, setUserName] = useState('Hugo');
   return (
     <Container>
       <Scroll>
-        <Text>Home</Text>
+        <Title>Welcome to app template, {userName}!</Title>
         <Card height={100} title="testando" />
       </Scroll>
     </Container>
---
+++ src/pages/Dashboard/Home/home.styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+  text-align: center;
+  font-size: 26px;
+  font-weight: 500;
+  margin: 20px 0;
+`;
